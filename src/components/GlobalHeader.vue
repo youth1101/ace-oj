5a46ca34c976ae1a86aa55510145df4733fe507e
TaoCoder <curry2746@qq.com>
2026-04-24 19:24:23 +0800

    项目通用布局——实现通用菜单

--- src/components/GlobalHeader.vue
+++ src/components/GlobalHeader.vue
@@ -1,20 +1,37 @@
 <template>
   <div id="globalHeader">
-    <a-menu mode="horizontal" :default-selected-keys="['1']">
+    <a-menu mode="horizontal" :selected-keys="selectedKeys" @menu-item-click="doMenuClick">
       <a-menu-item key="0" :style="{ padding: 0, marginRight: '38px' }" disabled>
         <div class="title-bar">
           <!-- <img class="logo" src="../assets/logo.jpg" alt="" /> -->
           <div class="title">OnlieJudge</div>
         </div>
       </a-menu-item>
-      <a-menu-item key="1">Home</a-menu-item>
-      <a-menu-item key="2">Solution</a-menu-item>
-      <a-menu-item key="3">Cloud Service</a-menu-item>
-      <a-menu-item key="4">Cooperation</a-menu-item>
+      <a-menu-item v-for="item in routes" :key="item.path">
+        {{ item.name }}
+      </a-menu-item>
     </a-menu>
   </div>
 </template>
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { routes } from '../router/routes'
+import { useRouter } from 'vue-router'
+import { ref } from 'vue'
+
+const router = useRouter()
+
+// 默认的主页
+const selectedKeys = ref(['/'])
+
+// 路由跳转时，更新选中菜单项（让顶部菜单的高亮状态，自动跟随页面路由变化）
+router.afterEach((to, from, failure) => {
+  selectedKeys.value = [to.path]
+})
+
+const doMenuClick = (key: string) => {
+  router.push({ path: key })
+}
+</script>
 <style scoped>
 .title-bar {
   display: flex;
---
+++ src/router/routes.ts
@@ -0,0 +1,16 @@
+import type { RouteRecordRaw } from 'vue-router'
+export const routes: Array<RouteRecordRaw> = [
+  {
+    path: '/',
+    name: 'home',
+    // component: HomeView,
+  },
+  {
+    path: '/about',
+    name: 'about',
+    // route level code-splitting
+    // this generates a separate chunk (About.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    // component: () => import('../views/AboutView.vue'),
+  },
+]
