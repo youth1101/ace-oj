a4d389d6b610f53feca42312078ccdcb12383b36
TaoCoder <curry2746@qq.com>
2026-04-24 17:19:28 +0800

    通用布局

---
+++ src/layouts/BasicLayout.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="basiclayout">
+    <a-layout style="height: 400px">
+      <a-layout-header class="header">
+        <GlobalHeader />
+      </a-layout-header>
+      <a-layout-content class="content">
+        <route-view />
+      </a-layout-content>
+      <a-layout-footer class="footer">
+        <a href="https://github.com/youth1101/ace-oj" target="_blank"> 项目源码地址 </a>
+      </a-layout-footer>
+    </a-layout>
+  </div>
+</template>
+
+<script setup lang="ts">
+import GlobalHeader from '@/components/GlobalHeader.vue'
+</script>
+
+<style scoped>
+#basiclayout {
+}
+#basiclayout .header {
+  background-color: red;
+  margin-bottom: 16px;
+}
+#basiclayout .content {
+}
+#basiclayout .footer {
+}
+</style>
