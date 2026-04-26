166b6beafec49603d4d721593b44c0a7c107a117
TaoCoder <curry2746@qq.com>
2026-04-26 14:25:19 +0800

    commit

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,29 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import type { RouteRecordRaw } from 'vue-router'
 import { routes } from './routes'
+
+import { useUserStore } from '@/stores/user'
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes,
+  routes: routes as RouteRecordRaw[],
+})
+
+router.beforeEach((to, from, next) => {
+  // 获取用户状态
+  const userStore = useUserStore()
+
+  // 判断是否需要管理员权限
+  if (to.meta?.access === 'canAdmin') {
+    // 如果用户不是 admin → 跳转到无权限页面
+    if (userStore.loginUser?.role !== 'admin') {
+      next('/noAuth')
+      return
+    }
+  }
+
+  // 放行
+  next()
 })
 
 export default router
