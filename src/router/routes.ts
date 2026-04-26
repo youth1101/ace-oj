69a3c453a8d3587095680475891d20484c50641b
TaoCoder <curry2746@qq.com>
2026-04-26 13:59:28 +0800

    commit

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -2,12 +2,12 @@ import type { RouteRecordRaw } from 'vue-router'
 export const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
-    name: 'home',
+    name: '浏览题目',
     // component: HomeView,
   },
   {
     path: '/about',
-    name: 'about',
+    name: '关于我的',
     // route level code-splitting
     // this generates a separate chunk (About.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
